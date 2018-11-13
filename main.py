b129c4f0606c053087a568b1f93f1b77389e916f
Blob Freeman <ahmedsalim2207@gmail.com>
2018-11-13 21:38:49 +0100

    Changed it so that it has dictionaries instead of arrays and made an array out of the dictionaries

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from mutagen.flac import FLAC
 from mutagen.id3 import ID3
 from mutagen.mp4 import MP4
 
+list_of_song = []
 
 def directorychooser():
     directory = askdirectory()
@@ -19,7 +20,7 @@ def directorychooser():
             artist = ''.join(audiofile['artist'])
             album = ''.join(audiofile['album'])
             flac_metadata = {'title': title, 'artist': artist, 'album': album}
-            print(flac_metadata)
+            list_of_song.append(flac_metadata)
         elif files.endswith(".mp3"):
             realdir = os.path.realpath(files)
             audiofile = ID3(realdir)
@@ -28,7 +29,7 @@ def directorychooser():
             artist = audiofile['TPE1'].text[0]
             album = audiofile['TALB'].text[0]
             mp3_metadata = {'title': title, 'artist': artist, 'album': album}
-            print(mp3_metadata)
+            list_of_song.append(mp3_metadata)
         elif files.endswith(".ogg"):
             realdir = os.path.realpath(files)
             audiofile = mutagen.File(realdir)
@@ -37,7 +38,7 @@ def directorychooser():
             artist = ''.join(audiofile['artist'])
             album = ''.join(audiofile['album'])
             ogg_metadata = {'title': title, 'artist': artist, 'album': album}
-            print(ogg_metadata)
+            list_of_song.append(ogg_metadata)
         elif files.endswith(".mp4"):
             realdir4 = os.path.realpath(files)
             audiofile_3 = MP4(realdir4)
@@ -46,7 +47,9 @@ def directorychooser():
             artist = audiofile_3['artist']
             album = audiofile_3['album']
             mp4_metadata = {'title': title, 'artist': artist, 'album': album}
-            print(mp4_metadata)
+            list_of_song.append(mp4_metadata)
 
 
 directorychooser()
+
+print(list_of_song)
