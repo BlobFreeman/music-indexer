ecff423ada011e724014132b3b16d25c6e7ccbd4
Blob Freeman <ahmedsalim2207@gmail.com>
2018-11-13 21:18:35 +0100

    testing

--- main.py
+++ main.py
@@ -12,40 +12,40 @@ def directorychooser():
 
     for files in os.listdir(directory):
         if files.endswith(".flac"):
-            flac_metadata = ['title', 'artist', 'album']
             realdir = os.path.realpath(files)
             audiofile = FLAC(realdir)
             # title, artist, album
-            flac_metadata.insert(1, audiofile['title'])
-            flac_metadata.insert(3, audiofile['artist'])
-            flac_metadata.insert(5, audiofile['album'])
+            title = ''.join(audiofile['title'])
+            artist = ''.join(audiofile['artist'])
+            album = ''.join(audiofile['album'])
+            flac_metadata = {'title': title, 'artist': artist, 'album': album}
             print(flac_metadata)
         elif files.endswith(".mp3"):
-            mp3_metadata = ['title', 'artist', 'album']
-            realdir2 = os.path.realpath(files)
-            audiofile_1 = ID3(realdir2)
+            realdir = os.path.realpath(files)
+            audiofile = ID3(realdir)
             # TIT2, TPE1, TALB
-            mp3_metadata.insert(1, audiofile_1['TIT2'].text[0])
-            mp3_metadata.insert(3, audiofile_1['TPE1'].text[0])
-            mp3_metadata.insert(5, audiofile_1['TALB'].text[0])
+            title = audiofile['TIT2'].text[0]
+            artist = audiofile['TPE1'].text[0]
+            album = audiofile['TALB'].text[0]
+            mp3_metadata = {'title': title, 'artist': artist, 'album': album}
             print(mp3_metadata)
         elif files.endswith(".ogg"):
-            ogg_metadata = ['title', 'artist', 'album']
-            realdir3 = os.path.realpath(files)
-            audiofile_2 = mutagen.File(realdir3)
+            realdir = os.path.realpath(files)
+            audiofile = mutagen.File(realdir)
             # title, artist, album
-            ogg_metadata.insert(1, audiofile_2['title'])
-            ogg_metadata.insert(3, audiofile_2['artist'])
-            ogg_metadata.insert(5, audiofile_2['album'])
+            title = ''.join(audiofile['title'])
+            artist = ''.join(audiofile['artist'])
+            album = ''.join(audiofile['album'])
+            ogg_metadata = {'title': title, 'artist': artist, 'album': album}
             print(ogg_metadata)
         elif files.endswith(".mp4"):
-            mp4_metadata = ['title', 'artist', 'album']
             realdir4 = os.path.realpath(files)
             audiofile_3 = MP4(realdir4)
             # title, artist, album
-            mp4_metadata.insert(1, audiofile_3['title'])
-            mp4_metadata.insert(3, audiofile_3['artist'])
-            mp4_metadata.insert(5, audiofile_3['album'])
+            title = audiofile_3['title']
+            artist = audiofile_3['artist']
+            album = audiofile_3['album']
+            mp4_metadata = {'title': title, 'artist': artist, 'album': album}
             print(mp4_metadata)
 
 
